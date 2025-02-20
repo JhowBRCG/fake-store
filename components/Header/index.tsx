573c45c2f98522d95f2c14520df4fbec41edc017
Jhonathan <guarino90@gmail.com>
2025-02-20 20:18:35 -0300

    feat: add top-0 to Header component

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,7 @@ import Cart from "../Cart";
 
 export default function Header() {
   return (
-    <div className="fixed flex h-[75px] w-full items-center justify-between bg-slate-50 px-[16px]">
+    <div className="fixed top-0 flex h-[75px] w-full items-center justify-between bg-slate-50 px-[16px]">
       <p>STORE</p>
       <Cart />
     </div>
