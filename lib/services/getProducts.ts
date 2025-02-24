945219a6556793a404ef942f35ae129678ee2d9c
Jhonathan <guarino90@gmail.com>
2025-02-24 00:38:16 -0300

    refactor: reposition if statement in getProducts function

--- lib/services/getProducts.ts
+++ lib/services/getProducts.ts
@@ -4,9 +4,10 @@ export async function getProducts(currentPage: number, itemsPerPage: number) {
   const response = await fetch(
     `https://fakestoreapi.in/api/products?page=${currentPage}&limit=${itemsPerPage}`,
   );
-  const data = await response.json();
 
   if (!response.ok) throw new Error("Failed to fetch data");
+
+  const data = await response.json();
 
   return data.products as Promise<productProps[]>;
 }
