630152138431a18d4cfaa3864bc0c063af53762f
Jhonathan <guarino90@gmail.com>
2025-02-25 18:01:11 -0300

    feat: create ProductList component

---
+++ components/ProductList/index.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import ProductCard from "../ProductCard";
+import { productProps } from "@/lib/@types/productProps";
+
+type productListProps = {
+  products: productProps[];
+};
+
+export default function ProductList({ products }: productListProps) {
+  return (
+    <div className="mt-[20px] grid grid-cols-2 gap-[2px] overflow-hidden rounded-lg">
+      {products?.map((product: productProps) => (
+        <Link href="" key={product.id}>
+          <ProductCard product={product} />
+        </Link>
+      ))}
+    </div>
+  );
+}
