42379c1a05db21031b234173f13b7d915bcc803b
Jhonathan <guarino90@gmail.com>
2025-02-27 19:38:44 -0300

    feat: add Layout component to UI folder

---
+++ components/Layout/Header/index.tsx
@@ -0,0 +1,12 @@
+import Cart from "../../Cart";
+import SearchInput from "../../SearchInput";
+
+export default function Header() {
+  return (
+    <div className="fixed top-0 flex h-[75px] w-full items-center justify-between bg-slate-50 px-[16px]">
+      <p>STORE</p>
+      <SearchInput />
+      <Cart />
+    </div>
+  );
+}
