b38fb908ec2e15a81408946f03d03082d7b7bc35
Jhonathan <guarino90@gmail.com>
2025-02-27 19:24:45 -0300

    feat: add Pagination component, parameters and placeholderData to Products

--- components/Products/index.tsx
+++ components/Products/index.tsx
@@ -1,14 +1,20 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "next/navigation";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { getProducts } from "@/lib/services/getProducts";
 import ProductList from "../ProductList";
+import Pagination from "../Pagination";
 
 export default function Products() {
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get("page") || 1);
+
   const { data, isLoading } = useQuery({
-    queryKey: ["products"],
-    queryFn: () => getProducts(1),
+    queryKey: ["products", currentPage],
+    queryFn: () => getProducts(currentPage),
     refetchOnWindowFocus: false,
+    placeholderData: keepPreviousData,
   });
 
   if (isLoading) return <p>Loading...</p>;
@@ -16,6 +22,9 @@ export default function Products() {
   return (
     <section>
       <ProductList products={data || []} />
+      <nav className="mt-4">
+        <Pagination currentPage={currentPage} />
+      </nav>
     </section>
   );
 }
