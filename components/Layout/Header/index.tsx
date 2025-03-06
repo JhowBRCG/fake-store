77175d24f7c08a12cd21f36d4e7b3c1ee0f9ee79
Jhonathan <guarino90@gmail.com>
2025-03-06 19:29:35 -0300

    feat: add z-index to Header component

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -3,7 +3,7 @@ import SearchInput from "../../UI/SearchInput";
 
 export default function Header() {
   return (
-    <div className="fixed top-0 flex h-[75px] w-full items-center justify-between bg-slate-50 px-[16px]">
+    <div className="fixed top-0 z-10 flex h-[75px] w-full items-center justify-between bg-slate-50 px-[16px]">
       <p>STORE</p>
       <SearchInput />
       <Cart />
