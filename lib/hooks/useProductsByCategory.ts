8ba7ad8cf08380c7aecd7d6cd23e8563b4ba6da3
Jhonathan <guarino90@gmail.com>
2025-03-09 20:46:00 -0300

    feat: create useProductsByCategory hook

---
+++ lib/hooks/useProductsByCategory.ts
@@ -0,0 +1,11 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { getProductsByCategory } from "../services/getProductsByCategory";
+
+export function useProductsByCategory(category: string, currentPage: number) {
+  return useQuery({
+    queryKey: ["products", currentPage],
+    queryFn: () => getProductsByCategory(category, currentPage),
+    refetchOnWindowFocus: false,
+    placeholderData: keepPreviousData,
+  });
+}
