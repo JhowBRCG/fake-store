c68e9f7ae26dfd4f71a2b3fb2401b15151cb67cc
Jhonathan <guarino90@gmail.com>
2025-03-10 01:08:48 -0300

    feat: filter product list based on category selection

--- components/UI/FilterCategoryProduct/index.tsx
+++ components/UI/FilterCategoryProduct/index.tsx
@@ -3,11 +3,22 @@
 import { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
+import { CATEGORIES } from "@/config";
+import { useRouter, usePathname } from "next/navigation";
 
-export default function Select() {
+export default function FilterCategoryProduct() {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const pathName = usePathname();
+
+  const currentCategory = pathName.split("/")[2] || "Categories";
 
   const toggleSelect = () => setOpen(!open);
+
+  const handleCategory = (category: string) => {
+    router.push(`/category/${category}`);
+    setOpen(false);
+  };
 
   const Icon = open ? IoIosArrowUp : IoIosArrowDown;
 
@@ -17,23 +28,26 @@ export default function Select() {
         onClick={toggleSelect}
         className="flex w-full cursor-pointer items-center justify-between rounded-lg border-2 border-black bg-slate-50 px-2 py-1"
       >
-        <p className="text-center text-xs">Categories</p>
+        <p className="text-center text-xs capitalize">{currentCategory}</p>
         <Icon className="text-base" />
       </div>
       {open && (
         <div className="absolute mt-2 w-full rounded-lg bg-slate-50 p-2 px-0">
           <ul>
-            <li className="cursor-default px-2 py-1 text-xs hover:bg-slate-200">
-              Eletronics
-            </li>
-            <li className="cursor-default px-2 py-1 text-xs hover:bg-slate-200">
-              Games
-            </li>
-            <li className="cursor-default px-2 py-1 text-xs hover:bg-slate-200">
-              Audio
-            </li>
-            <li className="cursor-default px-2 py-1 text-xs hover:bg-slate-200">
-              Movies
+            {CATEGORIES.map((category) => (
+              <li
+                onClick={() => handleCategory(category)}
+                key={category}
+                className="cursor-default px-2 py-1 text-xs capitalize hover:bg-slate-200"
+              >
+                {category}
+              </li>
+            ))}
+            <li
+              onClick={() => router.push("/")}
+              className="cursor-default px-2 py-1 text-xs capitalize hover:bg-slate-200"
+            >
+              All
             </li>
           </ul>
         </div>
