0f8052f09b8b5171c62af3dc2d40f2019885c617
Jhonathan <guarino90@gmail.com>
2025-03-09 21:44:08 -0300

    feat: create ProductsCategory component

---
+++ components/Sections/ProductsCategory/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import ProductList from "@/components/ProductList";
+import Pagination from "@/components/UI/Pagination";
+import { useProductsByCategory } from "@/lib/hooks/useProductsByCategory";
+
+type ProductsCategoryProps = {
+  categoryName: string;
+};
+
+export default function ProductsCategory({
+  categoryName,
+}: ProductsCategoryProps) {
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get("page") || 1);
+  const { data, isLoading } = useProductsByCategory(categoryName, currentPage);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <section>
+      <ProductList products={data || []} />
+      <nav className="mt-4">
+        <Pagination currentPage={currentPage} />
+      </nav>
+    </section>
+  );
+}
