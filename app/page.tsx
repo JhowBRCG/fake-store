6fe66de33bfab01fb60f9718c504ab218f2fb3b3
Jhonathan <guarino90@gmail.com>
2025-03-09 23:26:20 -0300

    feat: add FilterProducts to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
-import Select from "@/components/UI/Select";
 import Products from "@/components/Sections/Products";
+import FilterProducts from "@/components/FilterProducts";
 
 export default function Home() {
   return (
     <main className="mt-[75px] px-[10px] py-[20px]">
-      <div className="flex items-center gap-2">
-        <Select />
-        <Select />
-        <Select />
-      </div>
+      <FilterProducts />
       <Products />
     </main>
   );
