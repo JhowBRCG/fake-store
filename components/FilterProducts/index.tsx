670db10098f45f53a9b12dca0c8a7b9673f7da27
Jhonathan <guarino90@gmail.com>
2025-03-09 21:45:03 -0300

    feat: create FilterProducts component

---
+++ components/FilterProducts/index.tsx
@@ -0,0 +1,10 @@
+import Select from "../UI/Select";
+
+export default function FilterProducts() {
+  return (
+    <div className="flex items-center gap-2">
+      <Select />
+      <Select />
+    </div>
+  );
+}
