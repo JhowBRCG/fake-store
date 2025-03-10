dfabe0ef4d6dec1e07d491f40a47a24435470900
Jhonathan <guarino90@gmail.com>
2025-03-10 01:00:57 -0300

    feat: add CATEGORIES to config.ts

--- config.ts
+++ config.ts
@@ -1,3 +1,12 @@
 export const API_URL = "https://fakestoreapi.in/api/products";
 
 export const ITEMS_PER_PAGE = 15;
+
+export const CATEGORIES = [
+  "tv",
+  "audio",
+  "laptop",
+  "mobile",
+  "gaming",
+  "appliances",
+];
