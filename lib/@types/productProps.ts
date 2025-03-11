b5a1493db43032fb7702e0783061372d725bc661
Jhonathan <guarino90@gmail.com>
2025-03-11 00:31:11 -0300

    feat: modify productProps type sctructure

--- lib/@types/productProps.ts
+++ lib/@types/productProps.ts
@@ -1,12 +1,8 @@
 export type productProps = {
-  brand: string;
-  category: string;
-  color?: string;
-  description: string;
-  discount?: number;
   id: number;
-  image: string;
-  model: string;
-  price: number;
   title: string;
+  description: string;
+  price: number;
+  brand: string;
+  images: string[];
 };
