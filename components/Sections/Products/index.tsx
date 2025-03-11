2db30b06cff126b3e2f82366a2bb0af05b0017a0
Jhonathan <guarino90@gmail.com>
2025-03-11 20:13:57 -0300

    feat: add totalPages prop to pagination component

--- components/Sections/Products/index.tsx
+++ components/Sections/Products/index.tsx
@@ -16,7 +16,7 @@ export default function Products() {
     <section>
       <ProductList products={data?.products || []} />
       <nav className="mt-4">
-        <Pagination currentPage={currentPage} />
+        <Pagination currentPage={currentPage} totalPages={data?.total || 0} />
       </nav>
     </section>
   );
