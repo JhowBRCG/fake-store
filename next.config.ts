829d17410d56578d9ab4379da04ed9f77d36abdc
Jhonathan <guarino90@gmail.com>
2025-03-11 00:33:58 -0300

    feat: update protocol and hostname in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "storage.googleapis.com",
+        hostname: "cdn.dummyjson.com",
       },
     ],
   },
