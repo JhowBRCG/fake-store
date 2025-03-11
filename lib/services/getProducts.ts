056a92aa3a6991217f2e27644e2b5d29b342e207
Jhonathan <guarino90@gmail.com>
2025-03-11 18:03:46 -0300

    feat: update API_URL and type API response with apiResponse

--- lib/services/getProducts.ts
+++ lib/services/getProducts.ts
@@ -1,14 +1,12 @@
-import { productProps } from "../@types/productProps";
+import { apiResponse } from "../@types/productProps";
 import { API_URL, ITEMS_PER_PAGE } from "@/config";
 
 export async function getProducts(currentPage: number) {
-  const response = await fetch(
-    `${API_URL}?page=${currentPage}&limit=${ITEMS_PER_PAGE}`,
-  );
+  const response = await fetch(`${API_URL}?limit=${ITEMS_PER_PAGE}`);
 
   if (!response.ok) throw new Error("Failed to fetch data");
 
   const data = await response.json();
 
-  return data.products as productProps[];
+  return data as apiResponse;
 }
