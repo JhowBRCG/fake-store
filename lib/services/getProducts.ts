5b875947865c31e5a7f813f68d000c75d735d860
Jhonathan <guarino90@gmail.com>
2025-03-11 18:43:28 -0300

    feat: add skip parameter and its calculation in getProducts

--- lib/services/getProducts.ts
+++ lib/services/getProducts.ts
@@ -2,7 +2,11 @@ import { apiResponse } from "../@types/productProps";
 import { API_URL, ITEMS_PER_PAGE } from "@/config";
 
 export async function getProducts(currentPage: number) {
-  const response = await fetch(`${API_URL}?limit=${ITEMS_PER_PAGE}`);
+  const skip = (currentPage - 1) * ITEMS_PER_PAGE;
+
+  const response = await fetch(
+    `${API_URL}?limit=${ITEMS_PER_PAGE}&skip=${skip}`,
+  );
 
   if (!response.ok) throw new Error("Failed to fetch data");
 
