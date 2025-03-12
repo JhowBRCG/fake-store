df77d40176591d4fbe1818fabb36f6e5847a45c2
Jhonathan <guarino90@gmail.com>
2025-03-12 00:40:10 -0300

    fix: correctly calcute total pages in Pagination using calculateTotalPages function

--- components/Sections/ProductsCategory/index.tsx
+++ components/Sections/ProductsCategory/index.tsx
@@ -4,6 +4,7 @@ import { useSearchParams } from "next/navigation";
 import ProductList from "@/components/ProductList";
 import Pagination from "@/components/UI/Pagination";
 import { useProductsByCategory } from "@/lib/hooks/useProductsByCategory";
+import calculateTotalPages from "@/lib/utils/calculateTotalPages";
 
 type ProductsCategoryProps = {
   categoryName: string;
@@ -17,12 +18,13 @@ export default function ProductsCategory({
   const { data, isLoading } = useProductsByCategory(categoryName, currentPage);
 
   if (isLoading) return <p>Loading...</p>;
+  const totalPages = calculateTotalPages(data?.total || 0);
 
   return (
     <section>
       <ProductList products={data?.products || []} />
       <nav className="mt-4">
-        <Pagination currentPage={currentPage} totalPages={data?.total || 0} />
+        <Pagination currentPage={currentPage} totalPages={totalPages} />
       </nav>
     </section>
   );
