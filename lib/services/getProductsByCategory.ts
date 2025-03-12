6137bf263957536922ef4f614465cb4edfe49cfe
Jhonathan <guarino90@gmail.com>
2025-03-12 00:08:39 -0300

    feat: add skip parameter and apiResponse type

--- lib/services/getProductsByCategory.ts
+++ lib/services/getProductsByCategory.ts
@@ -1,17 +1,18 @@
-import { productProps } from "../@types/productProps";
+import { apiResponse } from "../@types/productProps";
 import { API_URL, ITEMS_PER_PAGE } from "@/config";
 
 export async function getProductsByCategory(
   category: string,
   currentPage: number,
 ) {
+  const skip = (currentPage - 1) * ITEMS_PER_PAGE;
   const response = await fetch(
-    `${API_URL}/category?type=${category}&page=${currentPage}&limit=${ITEMS_PER_PAGE}`,
+    `${API_URL}/category/${category}?limit=${ITEMS_PER_PAGE}&skip=${skip}`,
   );
 
   if (!response.ok) throw new Error("Failed to fetch data");
 
   const data = await response.json();
 
-  return data.products as productProps[];
+  return data as apiResponse;
 }
