9a73f4bf1c0c8ba6cd24dbe1c2979cc29c82a2e4
Jhonathan <guarino90@gmail.com>
2025-03-13 00:11:39 -0300

    refactor: add useFilterCategoryProduct hook and remove old code

--- components/UI/FilterCategoryProduct/index.tsx
+++ components/UI/FilterCategoryProduct/index.tsx
@@ -1,39 +1,32 @@
 "use client";
 
-import { useState } from "react";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { CATEGORIES } from "@/config";
-import { useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { formatCategory } from "@/lib/utils/formatCategory";
+import { useFilterCategoryProduct } from "@/lib/hooks/useFilterCategoryProduct";
 
 export default function FilterCategoryProduct() {
-  const [open, setOpen] = useState(false);
-  const router = useRouter();
   const pathName = usePathname();
+  const { toggleFilter, handleCategory, isFilterOpen, allProducts } =
+    useFilterCategoryProduct();
 
   const currentCategory = formatCategory(
     pathName.split("/")[2] || "Categories",
   );
 
-  const toggleSelect = () => setOpen(!open);
-
-  const handleCategory = (category: string) => {
-    router.push(`/category/${category}`);
-    setOpen(false);
-  };
-
-  const Icon = open ? IoIosArrowUp : IoIosArrowDown;
+  const Icon = isFilterOpen ? IoIosArrowUp : IoIosArrowDown;
 
   return (
     <div className="relative w-full max-w-[50%]">
       <div
-        onClick={toggleSelect}
+        onClick={toggleFilter}
         className="flex w-full cursor-pointer items-center justify-between rounded-lg border-2 border-black bg-slate-50 px-2 py-1"
       >
         <p className="text-center text-xs capitalize">{currentCategory}</p>
         <Icon className="text-base" />
       </div>
-      {open && (
+      {isFilterOpen && (
         <div className="absolute mt-2 w-full rounded-lg bg-slate-50 p-2 px-0">
           <ul>
             {CATEGORIES.map((category) => (
@@ -46,7 +39,7 @@ export default function FilterCategoryProduct() {
               </li>
             ))}
             <li
-              onClick={() => router.push("/")}
+              onClick={allProducts}
               className="cursor-default px-2 py-1 text-xs capitalize hover:bg-slate-200"
             >
               All
