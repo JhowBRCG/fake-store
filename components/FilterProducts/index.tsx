e28a58e60bf1094bb1025c26a114ac8127530eca
Jhonathan <guarino90@gmail.com>
2025-03-15 01:09:04 -0300

    feat: add SortProductByPrice to FilterProducts

--- components/FilterProducts/index.tsx
+++ components/FilterProducts/index.tsx
@@ -1,9 +1,11 @@
 import FilterCategoryProduct from "../UI/FilterCategoryProduct";
+import SortProductByPrice from "../UI/SortProductByPrice";
 
 export default function FilterProducts() {
   return (
     <div className="flex items-center gap-2">
       <FilterCategoryProduct />
+      <SortProductByPrice />
     </div>
   );
 }
