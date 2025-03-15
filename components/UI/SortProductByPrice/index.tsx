cca1f11bfecdad42ed46a003eb2e5cd9abe5378c
Jhonathan <guarino90@gmail.com>
2025-03-15 01:07:56 -0300

    feat: create SortProductByPrice component

---
+++ components/UI/SortProductByPrice/index.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { useSortProductByPrice } from "@/lib/hooks/useSortProductByPrice";
+
+const sortItems = ["asc", "desc"];
+
+export default function SortProductByPrice() {
+  const {
+    isFilterOpen,
+    toggleFilter,
+    handleSortFilter,
+    allItems,
+    getCurrentSortOrder,
+  } = useSortProductByPrice();
+
+  const Icon = isFilterOpen ? IoIosArrowUp : IoIosArrowDown;
+
+  return (
+    <div className="relative w-full max-w-[50%]">
+      <div
+        onClick={toggleFilter}
+        className="flex w-full cursor-pointer items-center justify-between rounded-lg border-2 border-black bg-slate-50 px-2 py-1"
+      >
+        <p className="text-center text-xs">{getCurrentSortOrder()}</p>
+        <Icon className="text-base" />
+      </div>
+
+      {isFilterOpen && (
+        <div className="absolute mt-2 w-full rounded-lg bg-slate-50 p-2 px-0">
+          <ul>
+            {sortItems.map((item) => (
+              <li
+                className="cursor-default px-2 py-1 text-xs capitalize hover:bg-slate-200"
+                key={item}
+                onClick={() => handleSortFilter(item)}
+              >
+                {item === "asc" ? "ascendent" : "descendent"}
+              </li>
+            ))}
+            <li
+              onClick={allItems}
+              className="cursor-default px-2 py-1 text-xs capitalize hover:bg-slate-200"
+            >
+              All
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
