10d52e68f2ab684bc62418051ffc12bc8993f325
Jhonathan <guarino90@gmail.com>
2025-03-15 20:11:40 -0300

    refactor: remove optional parameters from useProducts

--- lib/hooks/useProducts.ts
+++ lib/hooks/useProducts.ts
@@ -1,11 +1,7 @@
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { getProducts } from "../services/getProducts";
 
-export function useProducts(
-  currentPage: number,
-  sort?: string,
-  order?: string,
-) {
+export function useProducts(currentPage: number, sort: string, order: string) {
   return useQuery({
     queryKey: ["products", currentPage, sort, order],
     queryFn: () => getProducts(currentPage, sort, order),
