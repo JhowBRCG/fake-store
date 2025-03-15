882c28e9070e65e8c62320d6535045c187b616bb
Jhonathan <guarino90@gmail.com>
2025-03-15 00:19:50 -0300

    feat: add sort and order parameters to useProducts

--- lib/hooks/useProducts.ts
+++ lib/hooks/useProducts.ts
@@ -1,10 +1,14 @@
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { getProducts } from "../services/getProducts";
 
-export function useProducts(currentPage: number) {
+export function useProducts(
+  currentPage: number,
+  sort?: string,
+  order?: string,
+) {
   return useQuery({
-    queryKey: ["products", currentPage],
-    queryFn: () => getProducts(currentPage),
+    queryKey: ["products", currentPage, sort, order],
+    queryFn: () => getProducts(currentPage, sort, order),
     refetchOnWindowFocus: false,
     placeholderData: keepPreviousData,
   });
