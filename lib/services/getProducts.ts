add7268d53f76a36f0b2f153a87a4d4c66a145c7
Jhonathan <guarino90@gmail.com>
2025-03-15 00:18:46 -0300

    feat: add sort and order parameters to getProducts service

--- lib/services/getProducts.ts
+++ lib/services/getProducts.ts
@@ -1,11 +1,15 @@
 import { apiResponse } from "../@types/productProps";
 import { API_URL, ITEMS_PER_PAGE } from "@/config";
 
-export async function getProducts(currentPage: number) {
+export async function getProducts(
+  currentPage: number,
+  sort?: string,
+  order?: string,
+) {
   const skip = (currentPage - 1) * ITEMS_PER_PAGE;
 
   const response = await fetch(
-    `${API_URL}?limit=${ITEMS_PER_PAGE}&skip=${skip}`,
+    `${API_URL}?limit=${ITEMS_PER_PAGE}&skip=${skip}&sortBy=${sort || ""}&order=${order || ""}`,
   );
 
   if (!response.ok) throw new Error("Failed to fetch data");
