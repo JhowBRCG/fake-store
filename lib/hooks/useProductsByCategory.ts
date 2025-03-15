2c6ec6269f81ac82ffaca0c630d55b5190bc9c80
Jhonathan <guarino90@gmail.com>
2025-03-15 19:51:48 -0300

    feat: add sort and order parameters to useProductsByCategory

--- lib/hooks/useProductsByCategory.ts
+++ lib/hooks/useProductsByCategory.ts
@@ -1,10 +1,15 @@
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { getProductsByCategory } from "../services/getProductsByCategory";
 
-export function useProductsByCategory(category: string, currentPage: number) {
+export function useProductsByCategory(
+  category: string,
+  currentPage: number,
+  sort: string,
+  order: string,
+) {
   return useQuery({
-    queryKey: ["products", currentPage, category],
-    queryFn: () => getProductsByCategory(category, currentPage),
+    queryKey: ["products", currentPage, category, sort, order],
+    queryFn: () => getProductsByCategory(category, currentPage, sort, order),
     refetchOnWindowFocus: false,
     placeholderData: keepPreviousData,
   });
