d20ddda5b9d84c6662cd163e04956c6e8a732516
Jhonathan <guarino90@gmail.com>
2025-03-16 20:47:47 -0300

    refactor: rename SearchInput to SearchBar and update imports

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -1,11 +1,11 @@
 import Cart from "../../UI/Cart";
-import SearchInput from "../../UI/SearchInput";
+import SearchBar from "../../UI/SearchBar";
 
 export default function Header() {
   return (
     <div className="fixed top-0 z-10 flex h-[75px] w-full items-center justify-between bg-slate-50 px-[16px]">
       <p>STORE</p>
-      <SearchInput />
+      <SearchBar />
       <Cart />
     </div>
   );
