ef74527660fed9b9a24d76544f99bd906d802490
Jhonathan <guarino90@gmail.com>
2025-03-17 00:09:06 -0300

    feat: add search query logic to SearchBar component

--- components/UI/SearchBar/index.tsx
+++ components/UI/SearchBar/index.tsx
@@ -1,9 +1,32 @@
-import { IoMdSearch } from "react-icons/io";
+"use client";
 
-export default function SearchInput() {
+import { IoMdSearch } from "react-icons/io";
+import { useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
+export default function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const handleSubmitSearchQuery = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!searchQuery) return;
+
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("q", searchQuery.toString());
+    router.push(`?${params.toString()}`);
+
+    setSearchQuery("");
+  };
+
   return (
-    <div className="relative">
+    <form className="relative" onSubmit={handleSubmitSearchQuery}>
       <input
+        onChange={(e) => setSearchQuery(e.target.value)}
+        value={searchQuery}
+        name="search-bar"
         type="text"
         className="h-[35px] w-[215px] rounded-md pl-4 pr-8 focus:outline-1 focus:outline-black"
       />
@@ -11,6 +34,6 @@ export default function SearchInput() {
         <span className="absolute left-0 h-[0.1px] w-full -translate-x-[16px] rotate-90 bg-slate-200" />
         <IoMdSearch className="absolute inset-0 m-auto h-fit w-fit text-lg text-slate-300" />
       </button>
-    </div>
+    </form>
   );
 }
