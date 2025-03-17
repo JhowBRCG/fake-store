4baf996f0350bd6c6ebe5a33235150f8f7940b6c
Jhonathan <guarino90@gmail.com>
2025-03-17 00:25:16 -0300

    feat: add query variable to conditionally render search results or all products

--- components/Sections/Products/index.tsx
+++ components/Sections/Products/index.tsx
@@ -4,6 +4,7 @@ import { useSearchParams } from "next/navigation";
 import ProductList from "@/components/ProductList";
 import Pagination from "@/components/UI/Pagination";
 import { useProducts } from "@/lib/hooks/useProducts";
+import { useProductsBySearch } from "@/lib/hooks/useProductsBySearch";
 import calculateTotalPages from "@/lib/utils/calculateTotalPages";
 
 export default function Products() {
@@ -12,8 +13,12 @@ export default function Products() {
   const currentPage = Number(searchParams.get("page") || 1);
   const sort = String(searchParams.get("sort") || "");
   const order = String(searchParams.get("order") || "");
+  const query = String(searchParams.get("q") || "");
 
-  const { data, isLoading } = useProducts(currentPage, sort, order);
+  const searchResults = useProductsBySearch(query, currentPage, sort, order);
+  const allProducts = useProducts(currentPage, sort, order);
+
+  const { data, isLoading } = query ? searchResults : allProducts;
 
   const totalPages = calculateTotalPages(data?.total || 0);
 
