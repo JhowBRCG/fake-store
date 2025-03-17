85bfdb240ad8bba0dd332e8017e704563550109b
Jhonathan <guarino90@gmail.com>
2025-03-16 22:19:29 -0300

    feat: create getProductsBySearch service

---
+++ lib/services/getProductsBySearch.ts
@@ -0,0 +1,20 @@
+import { apiResponse } from "../@types/productProps";
+import { API_URL, ITEMS_PER_PAGE } from "@/config";
+
+export async function getProductsBySearch(
+  query: string,
+  currentPage: number,
+  sort: string,
+  order: string,
+) {
+  const skip = (currentPage - 1) * ITEMS_PER_PAGE;
+  const response = await fetch(
+    `${API_URL}/search?q=${query}&limit=${ITEMS_PER_PAGE}&skip=${skip}&sortBy=${sort}&order=${order}`,
+  );
+
+  if (!response.ok) throw new Error("Failed to fetch data");
+
+  const data = await response.json();
+
+  return data as apiResponse;
+}
