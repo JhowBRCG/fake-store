cba5d8bf21d3faef41f30304d35392dc44f284ce
Jhonathan <guarino90@gmail.com>
2025-03-17 00:02:38 -0300

    feat: create useProductsBySearch.ts

---
+++ lib/hooks/useProductsBySearch.ts
@@ -0,0 +1,17 @@
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { getProductsBySearch } from "../services/getProductsBySearch";
+
+export function useProductsBySearch(
+  query: string,
+  currentPage: number,
+  sort: string,
+  order: string,
+) {
+  return useQuery({
+    queryKey: ["productsSearch", query, currentPage, sort, order],
+    queryFn: () => getProductsBySearch(query, currentPage, sort, order),
+    refetchOnWindowFocus: false,
+    placeholderData: keepPreviousData,
+    enabled: !!query,
+  });
+}
