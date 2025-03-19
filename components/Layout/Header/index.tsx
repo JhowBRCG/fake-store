14be099c837e2b28479c696963836178889ff45d
Jhonathan <guarino90@gmail.com>
2025-03-19 19:21:55 -0300

    refactor: apply container-md max-width in Header

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -4,7 +4,7 @@ import SearchBar from "../../UI/SearchBar";
 export default function Header() {
   return (
     <div className="fixed top-0 z-10 h-[75px] w-full items-center justify-between bg-slate-50">
-      <div className="m-auto flex h-full w-full max-w-[700px] items-center justify-between px-[16px]">
+      <div className="max-w-container-md m-auto flex h-full w-full items-center justify-between px-[16px]">
         <p>STORE</p>
         <SearchBar />
         <Cart />
