5beef9477d274367e245248c0f7e89250f603ec1
Jhonathan <guarino90@gmail.com>
2025-03-19 16:09:40 -0300

    feat: add z-index to 10 and limit width with a wrapper in Header

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -3,10 +3,12 @@ import SearchBar from "../../UI/SearchBar";
 
 export default function Header() {
   return (
-    <div className="fixed top-0 z-10 flex h-[75px] w-full items-center justify-between bg-slate-50 px-[16px]">
-      <p>STORE</p>
-      <SearchBar />
-      <Cart />
+    <div className="fixed top-0 z-10 h-[75px] w-full items-center justify-between bg-slate-50">
+      <div className="m-auto flex h-full w-full max-w-[700px] items-center justify-between px-[16px]">
+        <p>STORE</p>
+        <SearchBar />
+        <Cart />
+      </div>
     </div>
   );
 }
