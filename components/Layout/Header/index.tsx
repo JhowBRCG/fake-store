3ac926357b389139658f8014d2314a5d3c67b3ea
Jhonathan <guarino90@gmail.com>
2025-03-19 21:54:43 -0300

    feat: apply container-2xl max-width for large screens in Header

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -4,7 +4,7 @@ import SearchBar from "../../UI/SearchBar";
 export default function Header() {
   return (
     <div className="fixed top-0 z-10 h-[75px] w-full items-center justify-between bg-slate-50">
-      <div className="max-w-container-md lg:max-w-container-lg m-auto flex h-full w-full items-center justify-between px-[16px]">
+      <div className="2xl:max-w-container-2xl max-w-container-md lg:max-w-container-lg m-auto flex h-full w-full items-center justify-between px-[16px]">
         <p>STORE</p>
         <SearchBar />
         <Cart />
