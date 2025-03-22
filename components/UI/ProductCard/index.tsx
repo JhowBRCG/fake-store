abfc685d32852f3962a7c388fcc4a5b5df81a863
Jhonathan <guarino90@gmail.com>
2025-03-22 18:24:08 -0300

    feat: add rating and stock status to ProductCard component

--- components/UI/ProductCard/index.tsx
+++ components/UI/ProductCard/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { productProps } from "@/lib/@types/productProps";
 import DiscountPercentage from "../DiscountPercentage";
+import { FaStar } from "react-icons/fa";
 
 type productCardProps = {
   product: productProps;
@@ -8,6 +9,7 @@ type productCardProps = {
 
 export default function ProductCard({ product }: productCardProps) {
   const discount = Math.floor(product.discountPercentage);
+  const rating = product.rating.toPrecision(2);
 
   return (
     <article className="h-full bg-white" key={product.id}>
@@ -22,10 +24,17 @@ export default function ProductCard({ product }: productCardProps) {
         />
       </div>
       <div className="px-[8px] py-[15px]">
-        <h2 className="text-sm">{product.title}</h2>
+        <h2>{product.title}</h2>
         <div className="flex items-center gap-1">
           <p className="mt-[7px]">${product.price}</p>
           <DiscountPercentage discount={discount} />
+        </div>
+        <div className="mt-[8px] flex items-center">
+          <div className="flex items-center gap-1 border-r pr-[4px]">
+            <FaStar className="text-xs text-[#FFBD1B]" />
+            <p className="text-xs">{rating}</p>
+          </div>
+          <p className="pl-[4px] text-xs">{product.stock} remaining</p>
         </div>
       </div>
     </article>
