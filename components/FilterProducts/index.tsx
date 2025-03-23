a11e62c520e28c360046303bf9b03bbecdeaa06d
Jhonathan <guarino90@gmail.com>
2025-03-23 00:56:37 -0300

    feat: add <p>, bg and padding to FilterProducts component

--- components/FilterProducts/index.tsx
+++ components/FilterProducts/index.tsx
@@ -3,7 +3,8 @@ import SortProductByPrice from "../UI/SortProductByPrice";
 
 export default function FilterProducts() {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-2 bg-neutral-100 p-[10px]">
+      <p className="text-xs text-[#555555]">Classify by</p>
       <CategoryFilter />
       <SortProductByPrice />
     </div>
