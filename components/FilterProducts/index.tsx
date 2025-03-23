6e32dd51e6b404d775dd492da8524565ce57ccf5
Jhonathan <guarino90@gmail.com>
2025-03-23 17:45:14 -0300

    feat: update bg-color and set props to CategoryFilter and SortProductByPrice in FilterProducts

--- components/FilterProducts/index.tsx
+++ components/FilterProducts/index.tsx
@@ -3,10 +3,10 @@ import SortProductByPrice from "../UI/SortProductByPrice";
 
 export default function FilterProducts() {
   return (
-    <div className="flex items-center gap-2 bg-neutral-100 p-[10px]">
+    <div className="flex items-center gap-2 bg-[#F5F5F5] p-[10px]">
       <p className="text-xs text-[#555555]">Classify by</p>
-      <CategoryFilter />
-      <SortProductByPrice />
+      <CategoryFilter className="w-full" />
+      <SortProductByPrice className="w-full" />
     </div>
   );
 }
