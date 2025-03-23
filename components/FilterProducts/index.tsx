97c4b0fc453efe79eff068a343f6ac21db55fb36
Jhonathan <guarino90@gmail.com>
2025-03-23 17:55:33 -0300

    feat: add SortProductByRelevance to FilterProducts

--- components/FilterProducts/index.tsx
+++ components/FilterProducts/index.tsx
@@ -1,10 +1,12 @@
 import CategoryFilter from "../UI/CategoryFilter";
 import SortProductByPrice from "../UI/SortProductByPrice";
+import SortProductByRelevance from "../UI/SortProductByRelevance";
 
 export default function FilterProducts() {
   return (
     <div className="flex items-center gap-2 bg-[#F5F5F5] p-[10px]">
       <p className="text-xs text-[#555555]">Classify by</p>
+      <SortProductByRelevance className="full" />
       <CategoryFilter className="w-full" />
       <SortProductByPrice className="w-full" />
     </div>
