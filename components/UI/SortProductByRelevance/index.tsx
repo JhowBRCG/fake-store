5c18cb757f2d29e773266ff3acf94b9498a8a988
Jhonathan <guarino90@gmail.com>
2025-03-23 22:16:44 -0300

    feat: add useSortByRelevance to SortProductByRelevance component

--- components/UI/SortProductByRelevance/index.tsx
+++ components/UI/SortProductByRelevance/index.tsx
@@ -1,15 +1,22 @@
 "use client";
 
 import { cn } from "@/lib/utils/cn";
+import { useSortByRelevance } from "@/lib/hooks/useSortByRelevance";
 
 export default function SortProductByPrice({
   className,
 }: {
   className: string;
 }) {
+  const { handleRelevanceSort, isRelevanceActive } = useSortByRelevance();
+
+  const relevanceColor = isRelevanceActive && "bg-red-400 text-white";
+
   return (
-    <div className={`relative ${cn(className)}`}>
-      <div className="flex w-full cursor-pointer items-center justify-between rounded-lg border-black bg-white px-2 py-[5px]">
+    <div className={`relative ${cn(className)}`} onClick={handleRelevanceSort}>
+      <div
+        className={`flex w-full cursor-pointer items-center justify-between rounded-lg border-black bg-white ${cn(relevanceColor)} px-2 py-[5px]`}
+      >
         <p className="text-center text-xs">Relevance</p>
       </div>
     </div>
