328ea40d482fbc83733c3f4c61e0be884b81a1b0
Jhonathan <guarino90@gmail.com>
2025-03-25 19:11:06 -0300

    refactor: rename relevanceColor variable to activateRelevanceColor in SortProductByRelevance

--- components/UI/SortProductByRelevance/index.tsx
+++ components/UI/SortProductByRelevance/index.tsx
@@ -10,14 +10,14 @@ export default function SortProductByPrice({
 }) {
   const { handleRelevanceSort, isRelevanceActive } = useSortByRelevance();
 
-  const relevanceColor = isRelevanceActive
+  const activateRelevanceClass = isRelevanceActive
     ? "bg-red-400 text-white"
     : "bg-white";
 
   return (
     <div className={`relative ${cn(className)}`} onClick={handleRelevanceSort}>
       <div
-        className={`flex w-full cursor-pointer items-center justify-between rounded-lg border-black ${cn(relevanceColor)} px-2 py-[5px]`}
+        className={`flex w-full cursor-pointer items-center justify-between rounded-lg border-black ${cn(activateRelevanceClass)} px-2 py-[5px]`}
       >
         <p className="text-center text-xs">Relevance</p>
       </div>
