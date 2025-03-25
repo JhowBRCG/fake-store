7e0f9621c279d3c8b8a475618606c7f3e7f014cd
Jhonathan <guarino90@gmail.com>
2025-03-24 23:38:56 -0300

    feat: create CategoryDrawer component

---
+++ components/UI/CategoryDrawer/index.tsx
@@ -0,0 +1,32 @@
+import { CATEGORIES } from "@/config";
+import { formatCategory } from "@/lib/utils/formatCategory";
+
+export default function SideBar({
+  handleCategory,
+  showAllProducts,
+}: {
+  handleCategory: (category: string) => void;
+  showAllProducts: () => void;
+}) {
+  return (
+    <nav className="absolute bottom-0 left-0 z-10 w-svw overflow-y-scroll bg-neutral-200">
+      <ul className="grid grid-cols-2">
+        {CATEGORIES.map((category) => (
+          <li
+            className="cursor-pointer text-nowrap border border-neutral-400 p-1 text-center text-sm capitalize hover:bg-red-400 hover:text-white"
+            key={category}
+            onClick={() => handleCategory(category)}
+          >
+            {formatCategory(category)}
+          </li>
+        ))}
+        <li
+          className="col-span-2 cursor-pointer border-t border-neutral-400 p-1 text-center text-sm hover:bg-red-400 hover:text-white"
+          onClick={showAllProducts}
+        >
+          All
+        </li>
+      </ul>
+    </nav>
+  );
+}
