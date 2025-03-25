c717e0b3ebf88a63ac1533e2c631adf31f8e34e6
Jhonathan <guarino90@gmail.com>
2025-03-24 23:43:23 -0300

    feat: remove old dropdown for category items and in place add CategoryDrawer in CategoryFilter

--- components/UI/CategoryFilter/index.tsx
+++ components/UI/CategoryFilter/index.tsx
@@ -1,52 +1,26 @@
 "use client";
 
-import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
-import { CATEGORIES } from "@/config";
-import { usePathname } from "next/navigation";
-import { formatCategory } from "@/lib/utils/formatCategory";
 import { useCategoryFilter } from "@/lib/hooks/useCategoryFilter";
 import { cn } from "@/lib/utils/cn";
+import CategoryDrawer from "../CategoryDrawer";
+import { useDisableScroll } from "@/lib/hooks/useDisableScroll";
 
 export default function CategoryFilter({ className }: { className: string }) {
-  const pathName = usePathname();
   const { toggleFilter, handleCategory, isFilterOpen, showAllProducts } =
     useCategoryFilter();
 
-  const currentCategory = formatCategory(
-    pathName.split("/")[2] || "Categories",
-  );
-
-  const Icon = isFilterOpen ? IoIosArrowUp : IoIosArrowDown;
+  useDisableScroll(isFilterOpen);
 
   return (
-    <div className={`relative w-full ${cn(className)}`}>
-      <div
-        onClick={toggleFilter}
-        className="flex w-full cursor-pointer items-center justify-between rounded-lg bg-white px-2 py-[5px]"
-      >
-        <p className="text-center text-xs capitalize">{currentCategory}</p>
-        <Icon className="text-base" />
+    <div className={`w-full ${cn(className)}`} onClick={toggleFilter}>
+      <div className="w-full cursor-pointer rounded-lg bg-white px-2 py-[5px]">
+        <p className="text-center text-xs capitalize">categories</p>
       </div>
       {isFilterOpen && (
-        <div className="absolute z-[5] mt-2 w-full rounded-lg bg-white p-2 px-0 shadow-md">
-          <ul className="relative grid grid-cols-2">
-            {CATEGORIES.map((category) => (
-              <li
-                onClick={() => handleCategory(category)}
-                key={category}
-                className="cursor-default border-b border-r px-2 py-2 text-xs capitalize hover:bg-slate-200"
-              >
-                {formatCategory(category)}
-              </li>
-            ))}
-            <li
-              onClick={showAllProducts}
-              className="col-span-2 cursor-default px-2 py-2 text-center text-xs capitalize hover:bg-slate-200"
-            >
-              All
-            </li>
-          </ul>
-        </div>
+        <CategoryDrawer
+          handleCategory={handleCategory}
+          showAllProducts={showAllProducts}
+        />
       )}
     </div>
   );
