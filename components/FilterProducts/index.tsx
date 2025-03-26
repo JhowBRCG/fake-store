38e56e8a00c83d0bf3bce21f5c675818f6c28013
Jhonathan <guarino90@gmail.com>
2025-03-25 21:31:10 -0300

    feat: add text-nowrap to <p> and set width to components in FilterProducts

--- components/FilterProducts/index.tsx
+++ components/FilterProducts/index.tsx
@@ -5,10 +5,10 @@ import SortProductByRelevance from "../UI/SortProductByRelevance";
 export default function FilterProducts() {
   return (
     <div className="flex items-center gap-2 bg-[#F5F5F5] p-[10px]">
-      <p className="text-xs text-[#555555]">Classify by</p>
-      <SortProductByRelevance className="full" />
-      <CategoryFilter className="w-full" />
-      <SortProductByPrice className="w-full" />
+      <p className="text-nowrap text-xs text-[#555555]">Classify by</p>
+      <SortProductByRelevance className="w-full max-w-[80px]" />
+      <CategoryFilter className="max-w-[80px]" />
+      <SortProductByPrice className="w-full max-w-[200px]" />
     </div>
   );
 }
