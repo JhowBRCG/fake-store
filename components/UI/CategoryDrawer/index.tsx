ce4a0559d857e12e7f5fcb5a7d5a60a67fec4ed0
Jhonathan <guarino90@gmail.com>
2025-03-25 23:41:59 -0300

    feat: redesign CategoryDrawer to start from left

--- components/UI/CategoryDrawer/index.tsx
+++ components/UI/CategoryDrawer/index.tsx
@@ -1,6 +1,7 @@
 import { CATEGORIES } from "@/config";
 import { formatCategory } from "@/lib/utils/formatCategory";
 import { cn } from "@/lib/utils/cn";
+import { FaWindowClose } from "react-icons/fa";
 
 export default function CategoryDrawer({
   handleCategory,
@@ -11,32 +12,37 @@ export default function CategoryDrawer({
   showAllProducts: () => void;
   isFilterOpen: boolean;
 }) {
-  const animation = isFilterOpen ? "translate-y-0" : "translate-y-full";
+  const animation = isFilterOpen ? "translate-x-0" : "-translate-x-full";
 
   return (
     <nav
       className={cn(
-        "fixed bottom-0 left-0 z-10 w-svw overflow-y-scroll bg-neutral-200 transition-transform duration-200",
+        "fixed left-0 top-0 z-10 h-screen w-[50%] bg-white shadow-md transition-transform duration-200 md:w-[30%] lg:w-[20%] 2xl:w-[15%]",
         animation,
       )}
     >
-      <ul className="grid grid-cols-2 md:grid-cols-3">
-        {CATEGORIES.map((category) => (
+      <div className="flex h-full flex-col">
+        <button className="w-full cursor-pointer border-b border-neutral-400 py-4">
+          <FaWindowClose className="m-auto text-center text-lg text-red-600" />
+        </button>
+        <ul className="flex-1 overflow-y-auto">
+          {CATEGORIES.map((category) => (
+            <li
+              className="cursor-pointer text-nowrap border-b border-neutral-400 py-2 text-center text-sm capitalize hover:bg-red-400 hover:text-white md:py-[6px]"
+              key={category}
+              onClick={() => handleCategory(category)}
+            >
+              {formatCategory(category)}
+            </li>
+          ))}
           <li
-            className="cursor-pointer text-nowrap border border-neutral-400 p-1 text-center text-sm capitalize hover:bg-red-400 hover:text-white"
-            key={category}
-            onClick={() => handleCategory(category)}
+            className="col-span-full cursor-pointer border-b border-neutral-400 py-2 text-center text-sm hover:bg-red-400 hover:text-white md:py-[6px]"
+            onClick={showAllProducts}
           >
-            {formatCategory(category)}
+            All
           </li>
-        ))}
-        <li
-          className="col-span-full cursor-pointer border-t border-neutral-400 p-1 text-center text-sm hover:bg-red-400 hover:text-white"
-          onClick={showAllProducts}
-        >
-          All
-        </li>
-      </ul>
+        </ul>
+      </div>
     </nav>
   );
 }
