8d411d2d35bc7d82f3b9d82b5050302d18cb6e6d
Jhonathan <guarino90@gmail.com>
2025-03-27 21:33:53 -0300

    feat: add path to href Link in ProductList

--- components/ProductList/index.tsx
+++ components/ProductList/index.tsx
@@ -10,7 +10,7 @@ export default function ProductList({ products }: productListProps) {
   return (
     <div className="mt-[20px] grid grid-cols-2 gap-[2px] overflow-hidden rounded-lg md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6">
       {products?.map((product: productProps) => (
-        <Link href="" key={product.id}>
+        <Link href={`/${product.id}`} key={product.id}>
           <ProductCard product={product} />
         </Link>
       ))}
