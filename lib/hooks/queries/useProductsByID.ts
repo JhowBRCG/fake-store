48612ca8b2c733fc363768091b5f77ee80cb38b9
Jhonathan <guarino90@gmail.com>
2025-03-27 21:42:27 -0300

    fix: set productID parameter to string

--- lib/hooks/queries/useProductsByID.ts
+++ lib/hooks/queries/useProductsByID.ts
@@ -1,7 +1,7 @@
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { getProductByID } from "@/lib/services/getProductByID";
 
-export function useProductsByID(productID: number) {
+export function useProductsByID(productID: string) {
   return useQuery({
     queryKey: ["product", productID],
     queryFn: () => getProductByID(productID),
--- lib/services/getProductByID.ts
+++ lib/services/getProductByID.ts
@@ -1,7 +1,7 @@
 import { productProps } from "../@types/productProps";
 import { API_URL } from "@/config";
 
-export async function getProductByID(productID: number) {
+export async function getProductByID(productID: string) {
   const response = await fetch(`${API_URL}/${productID}`);
 
   if (!response.ok) throw new Error("Failed to fetch data");
