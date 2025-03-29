00a3810a690cc7001ad2d1ffe4170b476abf8ed9
Jhonathan <guarino90@gmail.com>
2025-03-29 01:50:31 -0300

    feat: add Link component to Header

--- components/Layout/Header/index.tsx
+++ components/Layout/Header/index.tsx
@@ -1,11 +1,12 @@
 import Cart from "../../UI/Cart";
 import SearchBar from "../../UI/SearchBar";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="fixed top-0 z-10 h-[75px] w-full items-center justify-between bg-slate-50">
-      <div className="2xl:max-w-container-2xl max-w-container-md lg:max-w-container-lg m-auto flex h-full w-full items-center justify-between px-[16px]">
-        <p>STORE</p>
+      <div className="m-auto flex h-full w-full max-w-container-md items-center justify-between px-[16px] lg:max-w-container-lg 2xl:max-w-container-2xl">
+        <Link href="/">STORE</Link>
         <SearchBar />
         <Cart />
       </div>
