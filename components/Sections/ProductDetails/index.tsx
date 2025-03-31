016eb39ed42579a43dd9a878ea83c1c7899d9a97
Jhonathan <guarino90@gmail.com>
2025-03-31 00:12:27 -0300

    feat: add styles for different screen sizes in ProductDetails

--- components/Sections/ProductDetails/index.tsx
+++ components/Sections/ProductDetails/index.tsx
@@ -20,18 +20,18 @@ export default function ProductDetails({ productID }: { productID: string }) {
   if (isLoading) return <p>Loading...</p>;
 
   return (
-    <section className="rounded-sm bg-white">
+    <section className="rounded-sm bg-white lg:grid lg:grid-cols-[60%_40%]">
       <div className="flex items-center justify-center gap-1 py-7">
         <p className="text-sm">{ratingFormatted}</p>
         <RatingStars rating={rating} />
       </div>
-      <h1 className="text-center text-lg">{data?.title}</h1>
+      <h1 className="text-center text-lg lg:py-7">{data?.title}</h1>
       {data && (
         <Image
           src={data?.images[0]}
-          className="mx-auto pb-7 md:w-[270px]"
-          width={200}
-          height={200}
+          className="mx-auto h-[228px] object-contain pb-7 md:w-[270px] lg:row-span-4 lg:h-[500px] lg:w-[500px]"
+          width={400}
+          height={400}
           priority
           alt={data?.title}
         />
@@ -46,21 +46,21 @@ export default function ProductDetails({ productID }: { productID: string }) {
           <p>{data?.stock}</p>
         </div>
       </div>
-      <div className="p-5">
+      <div className="p-5 lg:col-start-2">
         {discount > 0 && (
           <p className="text-sm text-[#7F858D] line-through">{originalPrice}</p>
         )}
         <p className="text-2xl">{priceFormatted}</p>
       </div>
-      <div className="mb-[50px] flex gap-3 p-4">
-        <button className="flex-1 cursor-pointer rounded-md bg-[#81B29A] p-[9px] font-semibold">
-          BUY
-        </button>
+      <div className="col-start-2 mb-[50px] flex gap-3 p-4 lg:flex-col">
         <button className="flex-1 cursor-pointer rounded-md bg-[#F2CC8F] p-[9px] font-semibold">
           ADD TO CART
         </button>
+        <button className="flex-1 cursor-pointer rounded-md bg-[#81B29A] p-[9px] font-semibold">
+          BUY
+        </button>
       </div>
-      <div className="divide-y-2">
+      <div className="divide-y-2 lg:col-span-full">
         <Accordion title="description">
           <p>{data?.description}</p>
         </Accordion>
