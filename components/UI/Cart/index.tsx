e29c8b38e48215bbbbcc1c7b2fd1a0387ff091ce
Jhonathan <guarino90@gmail.com>
2025-04-02 23:43:48 -0300

    feat: add Link with path to /cart in Cart component

--- components/UI/Cart/index.tsx
+++ components/UI/Cart/index.tsx
@@ -1,12 +1,15 @@
 import { RiShoppingCartLine } from "react-icons/ri";
+import Link from "next/link";
 
 export default function Cart() {
   return (
-    <div className="relative cursor-pointer">
-      <RiShoppingCartLine className="text-2xl" />
-      <div className="absolute right-0 top-0 min-w-3 -translate-y-3 translate-x-2 rounded-full bg-slate-500 px-1">
-        <p className="text-center text-xs text-white">1</p>
+    <Link href="/cart">
+      <div className="relative cursor-pointer">
+        <RiShoppingCartLine className="text-2xl" />
+        <div className="absolute right-0 top-0 min-w-3 -translate-y-3 translate-x-2 rounded-full bg-slate-500 px-1">
+          <p className="text-center text-xs text-white">1</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
