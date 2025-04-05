f565956f852679f90d8819db08818208e02c590b
Jhonathan <guarino90@gmail.com>
2025-04-04 22:02:33 -0300

    feat: remove 'use client' directive and add CartHeader to cart page

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,18 +1,11 @@
-"use client";
-
-import { useSelector } from "react-redux";
-import { selectCartState } from "@/store/cartSlice";
 import CartItems from "@/components/cart/CartItems";
 import CartSummary from "@/components/cart/CartSummary";
+import CartHeader from "@/components/cart/CartHeader";
 
 export default function Cart() {
-  const cartState = useSelector(selectCartState);
-
   return (
-    <main className="my-[75px] px-[10px] py-[20px]">
-      <p className="text-center">
-        You have {cartState.totalProducts} products in cart
-      </p>
+    <main className="my-[90px] px-[10px] py-[20px]">
+      <CartHeader />
       <CartItems />
       <CartSummary />
     </main>
