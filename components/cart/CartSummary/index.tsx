fba89e4dba0c17df6d721dcf526dd744051ada47
Jhonathan <guarino90@gmail.com>
2025-04-04 21:07:44 -0300

    feat: create CartSummary component

---
+++ components/cart/CartSummary/index.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "@/store/cartSlice";
+import { selectCartState } from "@/store/cartSlice";
+import { formatPrice } from "@/lib/utils";
+
+export default function CartSummary() {
+  const dispatch = useDispatch();
+  const cartState = useSelector(selectCartState);
+  const formattedPrice = formatPrice(cartState.totalPrice);
+
+  return (
+    <div className="mt-[33px] bg-[#F5F5F5] p-4">
+      <p>
+        Total Price: <span className="text-red-400">{formattedPrice}</span>
+      </p>
+      <div className="mt-[22px] flex items-center justify-between">
+        <button
+          onClick={() => dispatch(clearCart())}
+          className="block cursor-pointer border border-red-400 px-[6px] py-[3px] text-sm text-red-400"
+        >
+          Clear Cart
+        </button>
+        <button className="block cursor-pointer rounded-md bg-white p-2 px-3 font-medium">
+          Checkout
+        </button>
+      </div>
+    </div>
+  );
+}
