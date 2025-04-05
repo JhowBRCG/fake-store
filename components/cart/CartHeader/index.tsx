dfa9cc8848060dd68377ffe6279070349007d978
Jhonathan <guarino90@gmail.com>
2025-04-04 22:01:25 -0300

    feat: create CartHeader component

---
+++ components/cart/CartHeader/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { selectCartState } from "@/store/cartSlice";
+import { useSelector } from "react-redux";
+
+export default function CartHeader() {
+  const cartState = useSelector(selectCartState);
+  const totalProducts = cartState.totalProducts;
+
+  const noProductsMessage =
+    totalProducts === 0 && "You don't have any product in cart";
+  const singularMessage =
+    totalProducts === 1 && `You have ${totalProducts} product in cart`;
+  const pluralMessage =
+    totalProducts > 1 && `You have ${totalProducts} products in cart`;
+
+  return (
+    <p className="text-center">
+      {noProductsMessage} {singularMessage} {pluralMessage}
+    </p>
+  );
+}
