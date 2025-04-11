48d5d2d3aafea3f96376fff7bb4925430f209d92
Jhonathan <guarino90@gmail.com>
2025-04-10 23:55:57 -0300

    test: add unit test for home page

---
+++ app/__tests__/page.test.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/display-name */
+
+import { render, screen } from "@testing-library/react";
+import Home from "../page";
+
+// Mocka os dois componentes importados
+
+jest.mock("@/components/products/FilterProducts", () => () => (
+  <div data-testid="filter-products">FilterProducts Component</div>
+));
+
+jest.mock("@/components/sections/Products", () => () => (
+  <div data-testid="products">Products Component</div>
+));
+
+describe("Home Page", () => {
+  it("render FilterProducts and Products component", () => {
+    render(<Home />);
+
+    expect(screen.getByTestId("filter-products")).toBeInTheDocument();
+    expect(screen.getByTestId("products")).toBeInTheDocument();
+  });
+});
