75cf267306f4a01a84636eb9bd4f037fe0103d82
Jhonathan <guarino90@gmail.com>
2025-04-11 23:14:23 -0300

    test: add unit test for Header component

---
+++ components/Layout/Header/__tests__/Header.test.tsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/display-name */
+
+import { render, screen } from "@testing-library/react";
+import Header from "..";
+
+jest.mock("../../../ui/SearchBar", () => () => (
+  <div data-testid="search-bar">SearchBar component</div>
+));
+
+jest.mock("../../../ui/Cart", () => () => (
+  <div data-testid="cart">Cart component</div>
+));
+
+describe("Header", () => {
+  it("render all children", () => {
+    render(<Header />);
+
+    expect(screen.getByText("STORE")).toBeInTheDocument();
+    expect(screen.getByTestId("search-bar")).toBeInTheDocument();
+    expect(screen.getByTestId("cart")).toBeInTheDocument();
+  });
+});
