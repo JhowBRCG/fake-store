52d2d6ad757db5fbbbe761f50e084f456f89efa2
Jhonathan <guarino90@gmail.com>
2025-04-13 23:21:50 -0300

    test: add data-testid to search icon

--- components/UI/SearchBar/index.tsx
+++ components/UI/SearchBar/index.tsx
@@ -18,7 +18,10 @@ export default function SearchBar() {
       />
       <button className="absolute inset-0 my-auto ml-auto h-[35px] w-[32px] cursor-pointer rounded-r-md hover:bg-red-400">
         <span className="absolute left-0 h-[0.1px] w-full -translate-x-[16px] rotate-90 bg-slate-200" />
-        <IoMdSearch className="absolute inset-0 m-auto h-fit w-fit text-lg text-slate-300" />
+        <IoMdSearch
+          data-testid="search-icon"
+          className="absolute inset-0 m-auto h-fit w-fit text-lg text-slate-300"
+        />
       </button>
     </form>
   );
