229be21c9cab4e4b7d8b026a2ef4bab86e231c45
Jhonathan <guarino90@gmail.com>
2025-04-15 01:16:56 -0300

    test: add unit test for CategoryFilter component

---
+++ components/products/CategoryFilter/__tests__/CategoryFilter.test.tsx
@@ -0,0 +1,94 @@
+/* eslint-disable react/display-name */
+
+import { fireEvent, render, screen } from "@testing-library/react";
+import CategoryFilter from "..";
+
+const toggleFilterMock = jest.fn();
+const handleCategoryMock = jest.fn();
+const showAllProducts = jest.fn();
+
+const useCategoryFilterMock = jest.fn();
+
+jest.mock("@/lib/hooks/useCategoryFilter", () => ({
+  useCategoryFilter: () => useCategoryFilterMock(),
+}));
+
+jest.mock("../../CategoryDrawer", () => () => (
+  <div data-testid="category-drawer">CategoryDrawer component</div>
+));
+
+describe("CategoryFilter", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("shoud render the categories button", () => {
+    useCategoryFilterMock.mockImplementation(() => ({
+      toggleFilter: toggleFilterMock,
+      handleCategory: handleCategoryMock,
+      isFilterOpen: false,
+      showAllProducts: showAllProducts,
+    }));
+
+    render(<CategoryFilter className="test-class" />);
+
+    expect(screen.getByText("categories")).toBeInTheDocument();
+  });
+
+  it("should call toggleFilter on click", () => {
+    useCategoryFilterMock.mockImplementation(() => ({
+      toggleFilter: toggleFilterMock,
+      handleCategory: handleCategoryMock,
+      isFilterOpen: false,
+      showAllProducts: showAllProducts,
+    }));
+
+    render(<CategoryFilter className="test-class" />);
+
+    const wrapper = screen.getByTestId("wrapper");
+    fireEvent.click(wrapper);
+
+    expect(toggleFilterMock).toHaveBeenCalled();
+  });
+
+  it("should render CategoryDrawer component", () => {
+    useCategoryFilterMock.mockImplementation(() => ({
+      toggleFilter: toggleFilterMock,
+      handleCategory: handleCategoryMock,
+      isFilterOpen: false,
+      showAllProducts: showAllProducts,
+    }));
+
+    render(<CategoryFilter className="test-class" />);
+
+    expect(screen.getByTestId("category-drawer")).toBeInTheDocument();
+  });
+
+  it("should aplly the correct class when isFilterIsOpen is true", () => {
+    useCategoryFilterMock.mockImplementation(() => ({
+      toggleFilter: toggleFilterMock,
+      handleCategory: handleCategoryMock,
+      isFilterOpen: true,
+      showAllProducts: showAllProducts,
+    }));
+
+    render(<CategoryFilter className="test-class" />);
+    const wrapper = screen.getByText("categories").closest("div");
+
+    expect(wrapper).toHaveClass("bg-red-400 text-white");
+  });
+
+  it("should apply the correct class when isFilterOpen is false", () => {
+    useCategoryFilterMock.mockImplementation(() => ({
+      toggleFilter: toggleFilterMock,
+      handleCategory: handleCategoryMock,
+      isFilterOpen: false,
+      showAllProducts: showAllProducts,
+    }));
+
+    render(<CategoryFilter className="test-class" />);
+    const wrapper = screen.getByText("categories").closest("div");
+
+    expect(wrapper).toHaveClass("bg-white");
+  });
+});
