aa47ccd8c10dd350d5169901b73a054dfbe78c92
Jhonathan <guarino90@gmail.com>
2025-04-18 15:29:13 -0300

    test: rename tests for a better understanding and add a new expect

--- components/Sections/ProductDetails/ProductPrice/__tests__/ProductPrice.test.tsx
+++ components/Sections/ProductDetails/ProductPrice/__tests__/ProductPrice.test.tsx
@@ -2,22 +2,23 @@ import { render, screen } from "@testing-library/react";
 import ProductPrice from "..";
 
 describe("ProductPrice", () => {
-  it("should not show original price if there is no discount", () => {
+  it("should not render original price if there is no discount", () => {
     render(<ProductPrice discount={0} price={10} />);
 
     expect(screen.queryByTestId("original-price")).not.toBeInTheDocument();
   });
 
-  it("should show price if exist no discount", () => {
+  it("should show current price if there is no discount", () => {
     render(<ProductPrice discount={0} price={10} />);
 
     expect(screen.getByText("$10.00")).toBeInTheDocument();
   });
 
-  it("should show original price and the price with discount", () => {
+  it("should show original price and discounted price when disocunt is applied", () => {
     render(<ProductPrice discount={50} price={10} />);
 
     expect(screen.getByTestId("original-price")).toBeInTheDocument();
+    expect(screen.getByText("$20.00")).toBeInTheDocument();
     expect(screen.getByText("$10.00")).toBeInTheDocument();
   });
 });
