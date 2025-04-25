7d5a3b7f46aa9235156531a4284e0a8567a59961
Jhonathan <guarino90@gmail.com>
2025-04-24 23:50:00 -0300

    feat: add HeaderBanner component to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Products from "@/components/sections/Products";
 import FilterProducts from "@/components/products/FilterProducts";
+import HeaderBanner from "@/components/sections/HeaderBanner";
 
 export default function Home() {
   return (
     <main className="mt-[75px] px-[10px] py-[20px]">
+      <HeaderBanner />
       <FilterProducts />
       <Products />
     </main>
