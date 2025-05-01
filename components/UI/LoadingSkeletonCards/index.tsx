306b9590251f5bd652dc5e695af495860c43382d
Jhonathan <guarino90@gmail.com>
2025-05-01 19:02:10 -0300

    feat: pass number of cards props to LoadingSkeletonCards component

--- components/UI/LoadingSkeletonCards/index.tsx
+++ components/UI/LoadingSkeletonCards/index.tsx
@@ -1,7 +1,11 @@
-export default function LoadingSkeleton() {
+type LoadingSkeletonCards = {
+  cards: number;
+};
+
+export default function LoadingSkeleton({ cards }: LoadingSkeletonCards) {
   return (
     <div className="mt-[20px] grid grid-cols-2 gap-[2px] overflow-hidden rounded-lg md:grid-cols-4 lg:grid-cols-5 2xl:grid-cols-6">
-      {Array.from({ length: 15 }).map((_, i) => (
+      {Array.from({ length: cards }).map((_, i) => (
         <article className="h-full bg-white" key={i}>
           <div className="relative h-[200px] border-b">
             <p className="absolute inset-0 m-auto h-fit w-fit text-slate-600">
