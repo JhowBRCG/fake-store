f608458981fe7e659025ebe5c4a5cc9ff9d94839
Jhonathan <guarino90@gmail.com>
2025-05-02 18:14:08 -0300

    feat: add Suspense wrapper around SearchBar in Header

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -1,13 +1,16 @@
 import Cart from "../../ui/Cart";
 import SearchBar from "../../ui/SearchBar";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function Header() {
   return (
     <header className="fixed top-0 z-10 h-[75px] w-full items-center justify-between bg-slate-50">
       <div className="m-auto flex h-full w-full max-w-container-md items-center justify-between px-[16px] lg:max-w-container-lg 2xl:max-w-container-2xl">
         <Link href="/">STORE</Link>
-        <SearchBar />
+        <Suspense>
+          <SearchBar />
+        </Suspense>
         <Cart />
       </div>
     </header>
