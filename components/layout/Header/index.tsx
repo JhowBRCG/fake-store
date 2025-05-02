662c4950ebfd1a74607b4259a03f204f78a906e0
Jhonathan <guarino90@gmail.com>
2025-05-02 17:44:55 -0300

    refactor: replace <div> with <header> tag in Header component

--- components/layout/Header/index.tsx
+++ components/layout/Header/index.tsx
@@ -4,12 +4,12 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="fixed top-0 z-10 h-[75px] w-full items-center justify-between bg-slate-50">
+    <header className="fixed top-0 z-10 h-[75px] w-full items-center justify-between bg-slate-50">
       <div className="m-auto flex h-full w-full max-w-container-md items-center justify-between px-[16px] lg:max-w-container-lg 2xl:max-w-container-2xl">
         <Link href="/">STORE</Link>
         <SearchBar />
         <Cart />
       </div>
-    </div>
+    </header>
   );
 }
