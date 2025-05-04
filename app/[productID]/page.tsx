9deccd001a0b9d1b5959f7af4151dc49c4c073d9
Jhonathan <guarino90@gmail.com>
2025-05-03 23:46:27 -0300

    feat: add dynamic metadata to ProductIdPage route

--- app/[productID]/page.tsx
+++ app/[productID]/page.tsx
@@ -1,8 +1,20 @@
 import ProductDetails from "@/components/sections/ProductDetails";
+import { getProductByID } from "@/lib/services/getProductByID";
 
 type Props = {
   params: Promise<{ productID: string }>;
 };
+
+export async function generateMetadata({ params }: Props) {
+  const { productID } = await params;
+
+  const product = await getProductByID(productID);
+
+  return {
+    title: product.title,
+    description: product.description,
+  };
+}
 
 export default async function ProductIdPage({ params }: Props) {
   const { productID } = await params;
