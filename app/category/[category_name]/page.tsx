4907b30ea667d8d479314311c3f6943c3b2e1a10
Jhonathan <guarino90@gmail.com>
2025-05-04 20:23:08 -0300

    feat: add metadata to CategoryNamePage route

--- app/category/[category_name]/page.tsx
+++ app/category/[category_name]/page.tsx
@@ -1,9 +1,23 @@
+import { Metadata } from "next";
+import { formatCategory, capitalizeFirstLetter } from "@/lib/utils";
 import FilterProducts from "@/components/products/FilterProducts";
 import ProductsCategory from "@/components/sections/ProductsCategory";
 
 type Props = {
   params: Promise<{ category_name: string }>;
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { category_name } = await params;
+  const formattedCategory = capitalizeFirstLetter(
+    formatCategory(category_name),
+  );
+
+  return {
+    title: formattedCategory,
+    description: `Buy anything you desire in our ${formattedCategory} category`,
+  };
+}
 
 export default async function CategoryNamePage({ params }: Props) {
   const { category_name } = await params;
