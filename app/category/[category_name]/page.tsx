2daabe27739910f85ec262739c5c03b7fbcdb4cf
Jhonathan <guarino90@gmail.com>
2025-05-04 19:24:32 -0300

    refactor: add Props type to CategoryNamePage route

--- app/category/[category_name]/page.tsx
+++ app/category/[category_name]/page.tsx
@@ -1,11 +1,11 @@
 import FilterProducts from "@/components/products/FilterProducts";
 import ProductsCategory from "@/components/sections/ProductsCategory";
 
-export default async function CategoryNamePage({
-  params,
-}: {
+type Props = {
   params: Promise<{ category_name: string }>;
-}) {
+};
+
+export default async function CategoryNamePage({ params }: Props) {
   const { category_name } = await params;
 
   return (
